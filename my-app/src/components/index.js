02fc9c04892d4430306b028d6eb29b5b2d1d7540
Alex49er <151443937+Alex49er@users.noreply.github.com>
2024-04-27 10:28:47 +0100

    started writing functions for the load more button to work

---
+++ my-app/src/components/index.js
@@ -0,0 +1,19 @@
+import {useEffect, useState} from 'react';
+
+
+
+export default function LoadMoreData() {
+
+    const [loading, setLoading] = useState(false);
+    const [products, setProducts] = useState([]);
+    const [count, setCount] = useState(0);
+
+    async function fetchProducts(){
+
+    }
+    useEffect(()=>{
+        fetchProducts()
+    },[])
+
+    return <div className="container"></div>
+}
